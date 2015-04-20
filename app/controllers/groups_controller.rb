6b91052774e8278e61161e31f8a8fe2ea4508429
Alexander <sharrikk@gmail.com>
2015-04-20 21:38:26 +0300

    return static json

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,9 @@
 class GroupsController < ApplicationController
 
+  def get_groups
+  	@@data = File.read("app/assets/javascripts/groups.json")
+  	render :json => @@data
+  	# render json: {facultets: Facultet.all, groups: Group.all}
+  end
 
 end
